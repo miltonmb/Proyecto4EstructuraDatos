f8adb93d6e0525064c19f53008256de632bdf3f9
gabriel <gabo995@unitec.edu>
2018-02-12 15:02:06 -0600

    subiendo

--- Universidad/Experiencia de Usuario/tarea/js/script.js
+++ Universidad/Experiencia de Usuario/tarea/js/script.js
@@ -1,101 +1,101 @@
-var imagenes = [1,2,3,4,5];
+var images = [1,2,3,4,5];
     $("#right").click(function(){
-      for (var i = 0; i < imagenes.length; i++) {
-        if(imagenes[i]==15){
-          imagenes[i]=1;
+      for (var i = 0; i < images.length; i++) {
+        if(images[i]==15){
+          images[i]=1;
         }else{
-          imagenes[i]++;
+          images[i]++;
         }
       }
-      $("#imagen1").html("<img src="+"./src/img"+imagenes[0]+".jpg"+" alt="+"Data not found"+">");
-      $("#imagen2").html("<img src="+"./src/img"+imagenes[1]+".jpg"+" alt="+"Data not found"+">");
-      $("#imagen3").html("<img src="+"./src/img"+imagenes[2]+".jpg"+" alt="+"Data not found"+">");
-      $("#imagen4").html("<img src="+"./src/img"+imagenes[3]+".jpg"+" alt="+"Data not found"+">");
-      $("#imagen5").html("<img src="+"./src/img"+imagenes[4]+".jpg"+" alt="+"Data not found"+">");
-      $("#main").html("<img src="+"./src/img"+imagenes[2]+".jpg"+" alt="+"Data not found"+">");
+      $(".img1").html("<img src="+"pictures/img"+images[0]+".jpg"+" alt="+"Data not found"+">");
+      $(".img2").html("<img src="+"pictures/img"+images[1]+".jpg"+" alt="+"Data not found"+">");
+      $(".img3").html("<img src="+"pictures/img"+images[2]+".jpg"+" alt="+"Data not found"+">");
+      $(".img4").html("<img src="+"pictures/img"+images[3]+".jpg"+" alt="+"Data not found"+">");
+      $(".img5").html("<img src="+"pictures/img"+images[4]+".jpg"+" alt="+"Data not found"+">");
+      $("#main").html("<img src="+"pictures/img"+images[2]+".jpg"+" alt="+"Data not found"+">");
     });
 
     $("#left").click(function(){
-        for (var i = 0; i < imagenes.length; i++) {
-          if(imagenes[i]==1){
-            imagenes[i]=5;
+        for (var i = 0; i < images.length; i++) {
+          if(images[i]==1){
+            images[i]=15;
           }else{
-            imagenes[i]--;
+            images[i]--;
           }
         }
-      $("#imagen1").html("<img src="+"./src/img"+imagenes[0]+".jpg"+" alt="+"Data not found"+">");
-      $("#imagen2").html("<img src="+"./src/img"+imagenes[1]+".jpg"+" alt="+"Data not found"+">");
-      $("#imagen3").html("<img src="+"./src/img"+imagenes[2]+".jpg"+" alt="+"Data not found"+">");
-      $("#imagen4").html("<img src="+"./src/img"+imagenes[3]+".jpg"+" alt="+"Data not found"+">");
-      $("#imagen5").html("<img src="+"./src/img"+imagenes[4]+".jpg"+" alt="+"Data not found"+">");
-      $("#main").html("<img src="+"./src/img"+imagenes[2]+".jpg"+" alt="+"Data not found"+">");
+      $(".img1").html("<img src="+"pictures/img"+images[0]+".jpg"+" alt="+"Data not found"+">");
+      $(".img2").html("<img src="+"pictures/img"+images[1]+".jpg"+" alt="+"Data not found"+">");
+      $(".img3").html("<img src="+"pictures/img"+images[2]+".jpg"+" alt="+"Data not found"+">");
+      $(".img4").html("<img src="+"pictures/img"+images[3]+".jpg"+" alt="+"Data not found"+">");
+      $(".img5").html("<img src="+"pictures/img"+images[4]+".jpg"+" alt="+"Data not found"+">");
+      $("#main").html("<img src="+"pictures/img"+images[2]+".jpg"+" alt="+"Data not found"+">");
     });
 
-    $("#imagen5").click(function(){
+    $(".img5").click(function(){
         var j = 0;
         while (j<2) {
-          for (var i = 0; i < imagenes.length; i++) {
-            if(imagenes[i]==5){
-              imagenes[i]=1;
+          for (var i = 0; i < images.length; i++) {
+            if(images[i]==15){
+              images[i]=1;
             }else{
-              imagenes[i]++;
+              images[i]++;
             }
           }
           j++;
         }
-      $("#imagen1").html("<img src="+"./src/img"+imagenes[0]+".jpg"+" alt="+"Data not found"+">");
-      $("#imagen2").html("<img src="+"./src/img"+imagenes[1]+".jpg"+" alt="+"Data not found"+">");
-      $("#imagen3").html("<img src="+"./src/img"+imagenes[2]+".jpg"+" alt="+"Data not found"+">");
-      $("#imagen4").html("<img src="+"./src/img"+imagenes[3]+".jpg"+" alt="+"Data not found"+">");
-      $("#imagen5").html("<img src="+"./src/img"+imagenes[4]+".jpg"+" alt="+"Data not found"+">");
-      $("#main").html("<img src="+"./src/img"+imagenes[2]+".jpg"+" alt="+"Data not found"+">");
+      $(".img1").html("<img src="+"pictures/img"+images[0]+".jpg"+" alt="+"Data not found"+">");
+      $(".img2").html("<img src="+"pictures/img"+images[1]+".jpg"+" alt="+"Data not found"+">");
+      $(".img3").html("<img src="+"pictures/img"+images[2]+".jpg"+" alt="+"Data not found"+">");
+      $(".img4").html("<img src="+"pictures/img"+images[3]+".jpg"+" alt="+"Data not found"+">");
+      $(".img5").html("<img src="+"pictures/img"+images[4]+".jpg"+" alt="+"Data not found"+">");
+      $("#main").html("<img src="+"pictures/img"+images[2]+".jpg"+" alt="+"Data not found"+">");
     });
-    $("#imagen4").click(function(){
-          for (var i = 0; i < imagenes.length; i++) {
-            if(imagenes[i]==15){
-              imagenes[i]=1;
+    $(".img4").click(function(){
+          for (var i = 0; i < images.length; i++) {
+            if(images[i]==15){
+              images[i]=1;
             }else{
-              imagenes[i]++;
+              images[i]++;
             }
           }
-      $("#imagen1").html("<img src="+"./src/img"+imagenes[0]+".jpg"+" alt="+"Data not found"+">");
-      $("#imagen2").html("<img src="+"./src/img"+imagenes[1]+".jpg"+" alt="+"Data not found"+">");
-      $("#imagen3").html("<img src="+"./src/img"+imagenes[2]+".jpg"+" alt="+"Data not found"+">");
-      $("#imagen4").html("<img src="+"./src/img"+imagenes[3]+".jpg"+" alt="+"Data not found"+">");
-      $("#imagen5").html("<img src="+"./src/img"+imagenes[4]+".jpg"+" alt="+"Data not found"+">");
-      $("#main").html("<img src="+"./src/img"+imagenes[2]+".jpg"+" alt="+"Data not found"+">");
+      $(".img1").html("<img src="+"pictures/img"+images[0]+".jpg"+" alt="+"Data not found"+">");
+      $(".img2").html("<img src="+"pictures/img"+images[1]+".jpg"+" alt="+"Data not found"+">");
+      $(".img3").html("<img src="+"pictures/img"+images[2]+".jpg"+" alt="+"Data not found"+">");
+      $(".img4").html("<img src="+"pictures/img"+images[3]+".jpg"+" alt="+"Data not found"+">");
+      $(".img5").html("<img src="+"pictures/img"+images[4]+".jpg"+" alt="+"Data not found"+">");
+      $("#main").html("<img src="+"pictures/img"+images[2]+".jpg"+" alt="+"Data not found"+">");
     });
-    $("#imagen1").click(function(){
+    $(".img1").click(function(){
         var j = 0;
         while (j<2) {
-          for (var i = 0; i < imagenes.length; i++) {
-            if(imagenes[i]==1){
-              imagenes[i]=5;
+          for (var i = 0; i < images.length; i++) {
+            if(images[i]==1){
+              images[i]=15;
             }else{
-              imagenes[i]--;
+              images[i]--;
             }
           }
           j++;
         }
-   $("#imagen1").html("<img src="+"./src/img"+imagenes[0]+".jpg"+" alt="+"Data not found"+">");
-      $("#imagen2").html("<img src="+"./src/img"+imagenes[1]+".jpg"+" alt="+"Data not found"+">");
-      $("#imagen3").html("<img src="+"./src/img"+imagenes[2]+".jpg"+" alt="+"Data not found"+">");
-      $("#imagen4").html("<img src="+"./src/img"+imagenes[3]+".jpg"+" alt="+"Data not found"+">");
-      $("#imagen5").html("<img src="+"./src/img"+imagenes[4]+".jpg"+" alt="+"Data not found"+">");
-      $("#main").html("<img src="+"./src/img"+imagenes[2]+".jpg"+" alt="+"Data not found"+">");
+   $(".img1").html("<img src="+"pictures/img"+images[0]+".jpg"+" alt="+"Data not found"+">");
+      $(".img2").html("<img src="+"pictures/img"+images[1]+".jpg"+" alt="+"Data not found"+">");
+      $(".img3").html("<img src="+"pictures/img"+images[2]+".jpg"+" alt="+"Data not found"+">");
+      $(".img4").html("<img src="+"pictures/img"+images[3]+".jpg"+" alt="+"Data not found"+">");
+      $(".img5").html("<img src="+"pictures/img"+images[4]+".jpg"+" alt="+"Data not found"+">");
+      $("#main").html("<img src="+"pictures/img"+images[2]+".jpg"+" alt="+"Data not found"+">");
     });
-    $("#imagen2").click(function(){
-      for (var i = 0; i < imagenes.length; i++) {
-        if(imagenes[i]==1){
-          imagenes[i]=5;
+    $(".img2").click(function(){
+      for (var i = 0; i < images.length; i++) {
+        if(images[i]==1){
+          images[i]=15;
         }else{
-          imagenes[i]--;
+          images[i]--;
         }
       }
-   $("#imagen1").html("<img src="+"./src/img"+imagenes[0]+".jpg"+" alt="+"Data not found"+">");
-      $("#imagen2").html("<img src="+"./src/img"+imagenes[1]+".jpg"+" alt="+"Data not found"+">");
-      $("#imagen3").html("<img src="+"./src/img"+imagenes[2]+".jpg"+" alt="+"Data not found"+">");
-      $("#imagen4").html("<img src="+"./src/img"+imagenes[3]+".jpg"+" alt="+"Data not found"+">");
-      $("#imagen5").html("<img src="+"./src/img"+imagenes[4]+".jpg"+" alt="+"Data not found"+">");
-      $("#main").html("<img src="+"./src/img"+imagenes[2]+".jpg"+" alt="+"Data not found"+">");
+   $(".img1").html("<img src="+"pictures/img"+images[0]+".jpg"+" alt="+"Data not found"+">");
+      $(".img2").html("<img src="+"pictures/img"+images[1]+".jpg"+" alt="+"Data not found"+">");
+      $(".img3").html("<img src="+"pictures/img"+images[2]+".jpg"+" alt="+"Data not found"+">");
+      $(".img4").html("<img src="+"pictures/img"+images[3]+".jpg"+" alt="+"Data not found"+">");
+      $(".img5").html("<img src="+"pictures/img"+images[4]+".jpg"+" alt="+"Data not found"+">");
+      $("#main").html("<img src="+"pictures/img"+images[2]+".jpg"+" alt="+"Data not found"+">");
     });
